fd25efc2f77c0894a5c617efa6de70aaa2a367e4
Yohjit Chopra <103960257+Yohjit-2910@users.noreply.github.com>
2022-11-19 12:51:40 +0530

    Update file.py

--- file.py
+++ file.py
@@ -1,32 +1,32 @@
-import smbus
+import smbus		#important libraries.
 import time
 
 address = 0x23
-start = 0x01
+start = 0x01		#addresses used by the I2C protocol
 stop = 0x00
 reset = 0x07
 
-bus = smbus.SMBus(1)
+bus = smbus.SMBus(1)	# initializing the instance for the SMbus.
 
 def lightRead():
 	updatedAddress = bus.read_i2c_block_data(address, address)
-	value = lightConversion(updatedAddress)
+	value = lightConversion(updatedAddress)		#function to get values of light intensity in float
 	return value
 
 def lightConversion(newAddress):
 	conversion = ((newAddress[1] + (256 * newAddress[0]))/1.2)
-	return conversion
+	return conversion				# this fucntion will be used to convert the intensity values in understandable numbers
 
 
 while True:
-    light_intensity = lightRead()
-    print(light_intensity)
+    light_intensity = lightRead()			#read the intensity of light.
+    print(light_intensity)				# print intensity of light on shell
     
     if(light_intensity >= 2000):
-        print("Status: Too bright")
+        print("Status: Too bright")			
         print()
     elif(light_intensity >= 200 and light_intensity < 2000):
-        print("Status: Bright")
+        print("Status: Bright")				# condition to print the intensity of light as Too Bright, Bright, Medium, Dark, Too Dark
         print()
     elif(light_intensity >= 50 and light_intensity < 200):
         print("Status: Medium")
